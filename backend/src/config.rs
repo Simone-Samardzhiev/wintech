aed496e4de6c0803699d9073d46be5359db79d9c
simone <simonesamardzhiev@gmail.com>
2026-03-26 14:49:16 +0200

    feat(backend): Improve file serving and configuration

--- backend/src/config.rs
+++ backend/src/config.rs
@@ -5,12 +5,18 @@ use std::env;
 #[derive(Debug)]
 pub struct Config {
     pub address: String,
+    pub frontend_path: String,
 }
 
 impl Config {
     /// Function to load the configuration from environment variables.
     pub fn new() -> anyhow::Result<Self> {
         let address = env::var("ADDRESS").with_context(|| "Failed to get ADDRESS")?;
-        Ok(Self { address })
+        let dist_path = env::var("DIST_PATH").with_context(|| "Failed to get DIST_PATH")?;
+
+        Ok(Self {
+            address,
+            frontend_path: dist_path,
+        })
     }
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -7,17 +7,12 @@ mod config;
 async fn main() {
     dotenv::dotenv().ok();
     let cnf = config::Config::new().unwrap();
-    let dist_path = "../frontend/dist";
 
-    let serve_dir = ServeDir::new(dist_path)
-        .append_index_html_on_directories(true)
-        .fallback(ServeFile::new(format!("{}/index.html", dist_path)));
-
-    let router = Router::new()
-        .nest_service("/assets", ServeDir::new(format!("{}/assets", dist_path)))
-        .fallback_service(serve_dir);
+    let router = Router::new().fallback_service(ServeDir::new(&cnf.frontend_path).not_found_service(
+        ServeFile::new(std::path::Path::new(&cnf.frontend_path).join("index.html")),
+    ));
 
     let listener = tokio::net::TcpListener::bind(&cnf.address).await.unwrap();
-    println!("Serving frontend from {} on {}", dist_path, cnf.address);
+    println!("Serving frontend from {} on {}", cnf.frontend_path, cnf.address);
     axum::serve(listener, router).await.unwrap();
 }
