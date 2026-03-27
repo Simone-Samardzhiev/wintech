82eb377de3ab3e09c3b16d8021c152c9349b50f2
simone <simonesamardzhiev@gmail.com>
2026-03-27 16:06:02 +0200

    fix(migrations): Make user table columns not null

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users
 (
     id       UUID PRIMARY KEY,
-    name     VARCHAR(128) CHECK ( char_length(name) > 8 ),
-    email    VARCHAR(256) UNIQUE,
-    password VARCHAR(128)
+    name     VARCHAR(128) CHECK ( char_length(name) > 8 ) NOT NULL,
+    email    VARCHAR(256) UNIQUE                          NOT NULL,
+    password VARCHAR(128)                                 NOT NULL
 )
