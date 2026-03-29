6dc3d3666153572360ec12db9d7575fe06d8679c
simone <simonesamardzhiev@gmail.com>
2026-03-29 22:33:57 +0300

    refactor(backend): Change username length constraints to >= 8

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users
 (
     id       UUID PRIMARY KEY,
-    name     VARCHAR(128) CHECK ( char_length(name) > 8 ) NOT NULL,
+    name     VARCHAR(128) CHECK ( char_length(name) >= 8 ) NOT NULL,
     email    VARCHAR(256) UNIQUE                          NOT NULL,
     password VARCHAR(128)                                 NOT NULL
 )
